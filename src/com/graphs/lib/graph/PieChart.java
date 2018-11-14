aeea2a41eee62896e217414e6ccdfc815f1315a0
Łukasz Matysik <lukasz.matysik6@gmail.com>
2018-11-14 12:30:08 +0100

    Created class GraphData to enable storing data. Class PieChart modified to work properly with class GraphData.

--- src/com/graphs/lib/graph/PieChart.java
+++ src/com/graphs/lib/graph/PieChart.java
@@ -1,17 +1,18 @@
 package com.graphs.lib.graph;
 
-import com.graphs.lib.graph.element.*;
+import com.graphs.lib.graph.element.Arc;
+import com.graphs.lib.graph.element.Color;
+import com.graphs.lib.graph.element.Point;
 import processing.core.PConstants;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import java.util.concurrent.ThreadLocalRandom;
 
 public class PieChart extends Graph {
 
-    private List<Double> values;
-    private List<Double> ratios;
+    private List<GraphData<Double>> values = new ArrayList<>();
+    private List<GraphData<Double>> ratios = new ArrayList<>();
     private List<Arc> arcs;
 
     public PieChart(){
@@ -21,51 +22,60 @@ public class PieChart extends Graph {
         super(width, height);
     }
 
-    public PieChart(List<Double> values) {
-        this.values = values;
-        ratios = countRatio(values);
-        this.arcs = createArcs(ratios);
+    public PieChart(List<String> labels, List<Double> values) throws Exception {
+        if(labels.size() != values.size())
+            throw new Exception("Number of labels is different than number of values");
+        createValuesList(labels,values);
+        countRatios();
+        createArcs();
+
     }
 
     public PieChart(int width, int height, List<Double> values) {
         super(width, height);
-        this.values = values;
-        ratios = countRatio(values);
-        this.arcs = createArcs(ratios);
+
     }
 
-    private List<Double> countRatio(List<Double> values)
+    private void createValuesList(List<String> labels, List<Double> values){
+        for(int i = 0; i<labels.size(); i++)
+            this.values.add(new GraphData<Double>(labels.get(i), values.get(i)));
+    }
+
+    private void countRatios()
     {
         List<Double> ratios= new ArrayList<>();
         double sum = 0;
 
-        for (double value:values) {
-            sum += value;
+        for (GraphData<Double> element : values) {
+            sum += element.getData();
         }
-        for (double value:values) {
-            ratios.add(value/sum);
+        for (GraphData<Double> element : values) {
+            ratios.add(element.getData()/sum);
         }
         for (double value:ratios) {
             System.out.println(value);
         }
 
-        return ratios;
+        for(int i = 0; i<values.size(); i++){
+            this.ratios.add(new GraphData<>(values.get(i).getLabel(),ratios.get(i)));
+        }
     }
 
-    private List<Arc> createArcs(List<Double> ratios)
+    private void createArcs()
     {
-        List<Arc> arcs = new ArrayList<>();
+        List<Arc> createdArcs = new ArrayList<>();
         float sum = 0;
         int randomNum1,randomNum2,randomNum3;
         int minimum = min(width,height);
-        for(int i = 0; i<ratios.size();i++){
+        for(int i = 0; i < ratios.size();i++){
             randomNum1 = ThreadLocalRandom.current().nextInt(0, 255 + 1);
             randomNum2 = ThreadLocalRandom.current().nextInt(0, 255 + 1);
             randomNum3 = ThreadLocalRandom.current().nextInt(0, 255 + 1);
-            arcs.add(new Arc(this,new Point((int)(0.35*width),(int)(0.5*height)),(int)(0.6 * minimum),sum * 2 * PConstants.PI,(float)(sum+ratios.get(i))*PConstants.PI*2,0,new Color(randomNum1,randomNum2,randomNum3),new Color(0,0,0),1));
-            sum+=ratios.get(i);
+            createdArcs.add(new Arc(this,new Point((int)(0.35*width),(int)(0.5*height)),(int)(0.6 * minimum),
+                    sum * 2 * PConstants.PI,(float)((sum + ratios.get(i).getData()) * PConstants.PI*2),new Color(randomNum1,randomNum2,randomNum3),new Color(0,0,0),0,1));
+            sum+=ratios.get(i).getData();
         }
-        return arcs;
+        this.arcs = createdArcs;
     }
     private void drawArcs(List<Arc> arcs){
         for (Arc arc: arcs) {
