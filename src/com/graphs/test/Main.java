754a5ae7a15d25b3148e03868093da6b435f23b4
Kamil Drewniak <zibix12@gmail.com>
2018-11-14 12:32:23 +0100

    main

--- src/com/graphs/test/Main.java
+++ src/com/graphs/test/Main.java
@@ -1,11 +1,9 @@
 package com.graphs.test;
 
-import com.graphs.lib.graph.LukTest;
-import com.graphs.lib.graph.PieChart;
+import com.graphs.lib.LineChart;
 
 public class Main {
     public static void main(String[] args) {
-        LukTest pie = new LukTest(1280, 720);
-        pie.run();
+
     }
 }
