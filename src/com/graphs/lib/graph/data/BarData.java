1a1fd5144cc941a77f8bb025fe783cdac8bce1c2
Łukasz Matysik <lukasz.matysik6@gmail.com>
2019-01-09 14:00:38 +0100

    Change name of data class

--- src/com/graphs/lib/graph/data/BarData.java
+++ src/com/graphs/lib/graph/data/BarData.java
@@ -6,24 +6,19 @@ public class BarData {
 
     public BarData() {
     }
-
     public BarData(String label, double data) {
         this.data = data;
         this.label = label;
     }
-
     public double getData() {
         return data;
     }
-
     public void setData(double data) {
         this.data = data;
     }
-
     public String getLabel() {
         return label;
     }
-
     public void setLabel(String label) {
         this.label = label;
     }
